e7e1d60f77dec4afccc11232d0e7cdb0844c5638
Hoon Lee <yl.hoony@gmail.com>
2018-01-05 07:06:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    # original_string = params["string"]
+    # @reversed_string = original_string.reverse
+    puts params
     erb :reversed
   end
 
